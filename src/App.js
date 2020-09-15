2adc3fa721519638c0dc4d531c8b358d38e09b1f
Bicky Tamang <bickytamang101@gmail.com>
2020-09-15 11:25:41 +0545

    Social Card Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Posts from "./containers/Posts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  setId(id) {
+    return id;
+  }
+
+  render () {
+    return (
+      <Posts 
+        id={this.setId(2)}
+        comments={2} retweet={10} heart={10} />
+    );
+  } 
 }
 
 export default App;
